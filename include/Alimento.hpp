fc3a7b951daf8d64e8f4e03bb9c9042ffec3749d
1DavidRM <160290507+1DavidRM@users.noreply.github.com>
2024-02-20 02:10:15 +0000

    Version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,18 @@
+#pragma once;
+class Alimento
+{
+private:
+    /* data */
+    int Energia;
+public:
+    Alimento(int Energia) {
+        this->Energia=Energia;
+    }
+
+    ~Alimento() {}
+    int ExtraerEnergia(){
+        return Energia;
+    }
+
+    
+};
