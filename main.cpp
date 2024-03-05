a129cf4eae4eee96c095ccda6da4fe617ae9d01b
1DavidRM <160290507+1DavidRM@users.noreply.github.com>
2024-03-05 01:53:01 +0000

    Version5

--- main.cpp
+++ main.cpp
@@ -2,18 +2,39 @@
     using namespace std;
     #include <Mascota.hpp>
     #include <Alimento.hpp>
+    #include <list>
     #include <Dibujo.hpp>
     int main (int argc, char const *argv[]){
         Dibujo dibujo1("./assets/kokoa.txt");
         Dibujo dibujo2("./assets/auuu.txt");
         dibujo1.Dibujar();
         dibujo2.Dibujar();
-        Mascota m1;
-        Mascota m2;
-        Mascota m3;
+        list<Dibujo> dibujos;
+        // dibujos.push_front(dibujo1);
+        // dibujos.push_front(dibujo2);
+
+
+        Mascota m1,m2,m3;
+       
         Alimento alimento(5);
-        m1.Comer(alimento);
+        /*m1.Comer(alimento);
         m2.Comer(alimento);
-        m3.Comer(alimento);
-        cout<<"Mascota "<<m1.LeerEnergia()<<endl;
+        m3.Comer(alimento);*/
+        list<Mascota>mascota;
+        mascota.push_front(m1);
+        mascota.push_front(m2);
+        mascota.push_front(m3);
+
+        for (auto &&mascotai : mascota)
+        {
+            mascotai.Comer(alimento);
+        }
+        
+        
+        for (auto &&mascotai : mascota)
+        {
+            cout<<"Mascota "<<mascotai.LeerEnergia()<<endl;
+
+        }
+        
     }
